0bc06d79e591825ee09a231290526f8b426d4ffb
jincf999 <jincf999@gmail.com>
2021-05-05 01:47:02 +0800

    update

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,5 +1,5 @@
-// export const API_URL = "http://localhost:5000";
-// export const STRIPE_PUBLIC_KEY = "pk_test_v4MinEclx2Ib1eBQxxlOzrCC00HPib7Zpr";
+export const API_URL = "http://localhost:5000";
+export const STRIPE_PUBLIC_KEY = "pk_test_v4MinEclx2Ib1eBQxxlOzrCC00HPib7Zpr";
 
-export const API_URL = "https://api.postgradrecruits.com:3000";
-export const STRIPE_PUBLIC_KEY = "pk_live_51IJMHVG8x0lCISOR9fH1gpzvf3pF1khLp5T4fM6L8Fkxsy4IKFcH3BHnYvNgYzronIA4fs8MvNiDuu6zUTJfGVef00lQPeuxK6";
+// export const API_URL = "https://api.postgradrecruits.com:3000";
+// export const STRIPE_PUBLIC_KEY = "pk_live_51IJMHVG8x0lCISOR9fH1gpzvf3pF1khLp5T4fM6L8Fkxsy4IKFcH3BHnYvNgYzronIA4fs8MvNiDuu6zUTJfGVef00lQPeuxK6";
